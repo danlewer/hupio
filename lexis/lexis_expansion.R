4c2d9a5c859b07e3460e5d5fb2b5f1f3a1785f23
Dan Lewer <danlewer@users.noreply.github.com>
2021-06-11 16:43:09 +0100

    Update lexis_expansion.R

--- lexis/lexis_expansion.R
+++ lexis/lexis_expansion.R
@@ -1,9 +1,10 @@
 library(data.table)
+library(lubridate)
 
 #  lexis expansion function
 #  ........................
 
-lexis <- function(data, id = 'id', start_date = 'entry', end_date = 'exit', baseline = 'exposure_baseline', opioids = 'becomes_exposed', dob = 'dob', event = 'death_date', age_groups = 0:20 * 5, ng = 50) {
+lexis <- function(data, id = 'id', start_date = 'entry', end_date = 'exit', baseline = 'exposure_baseline', opioids = 'becomes_exposed', dob = 'dob', event = 'death_date', age_groups = c(0, 18, seq(25, 100, 5)), period_starts = c(10227, 11323, 12418, 13514, 14610, 15706, 16801), ng = 50) {
   nr <- seq_len(nrow(data))
   ind <- findInterval(nr, quantile(nr, 0:(ng-1)/ng))
   x <- split(data, ind)
@@ -14,8 +15,9 @@ lexis <- function(data, id = 'id', start_date = 'entry', end_date = 'exit', base
     days[, age_group := findInterval(age, age_groups)]
     days[, opioids := exposure == 1 | (!is.na(opioids) & day >= opioids)]
     days[, event := death_date == day & !is.na(death_date)]
+    days[, period := findInterval(day, period_starts)]
     print(paste0(y, '/', ng))
-    days[, .(follow_up = .N, event = any(event)), c('id', 'age_group', 'opioids')]
+    days[, .(follow_up = .N, event = any(event)), c('id', 'age_group', 'period', 'opioids')]
   })
   rbindlist(l)
 }
@@ -29,7 +31,7 @@ study_start <- as.integer(as.Date('1998-01-01'))
 study_end <- as.integer(as.Date('2018-10-30'))
 earliest_dob <- as.integer(as.Date('1960-01-01'))
 latest_dob <- as.integer(as.Date('1995-12-31'))
-youngest_entrant <- as.integer(18 * 365.25)
+youngest_entrant <- as.integer(19 * 365.25)
 study_duration <- study_end - study_start
 opioid_mortality_ratio <- 10
 proportion_comparison_become_exposed <- 0.03
@@ -63,8 +65,14 @@ dat$becomes_exposed[dat$becomes_exposed == 0 | dat$exposure_baseline == 1] <- NA
 #  create lexis-expanded data
 #  ..........................
 
-age_lims <- 0:10 * 10
-lexis_data <- lexis(dat, id = 'id', start_date = 'entry', end_date = 'exit', baseline = 'exposure_baseline', opioids = 'becomes_exposed', dob = 'dob', event = 'death_date', age_groups = age_lims)
+age_lims <- c(0, 18, seq(25, 100, 5))
+period_starts <- seq(1998, 2018, 3)
+period_starts <- make_date(year = period_starts, 1, 1)
+
+lexis_data <- lexis(dat, id = 'id', start_date = 'entry', end_date = 'exit', baseline = 'exposure_baseline', opioids = 'becomes_exposed', dob = 'dob', event = 'death_date', age_groups = age_lims, period_starts = as.integer(period_starts))
+lexis_data[, age_group := factor(age_group, seq_along(age_lims), age_lims)]
+lexis_data[, period := factor(period, seq_along(period_starts), year(period_starts))]
+lexis_data <- droplevels(lexis_data)
 
 # check total follow-up duration (should be TRUE)
 
@@ -83,6 +91,6 @@ smr[, .(observed = sum(deaths), expected = sum(expected), smr = sum(deaths) / su
 #  poisson model
 #  .............
 
-lexis_data[, age_group := factor(age_group, seq_along(age_lims), age_lims)]
-model <- glm(event ~ opioids + age_group + offset(log(follow_up)), data = lexis_data, family = 'poisson')
+model <- glm(event ~ opioids + age_group + period + offset(log(follow_up)), data = lexis_data, family = 'poisson')
 exp(cbind(coef(model), confint(model)))[-1,]
+
